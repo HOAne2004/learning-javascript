b9c473e84e439233eec46f4c29038d6a6c0a80bf
HOAn04 <lehuyhoan04@gmail.com>
2026-09-26 06:36:23 +0700

    learn: type of console

---
+++ src/console-command/console-command.js
@@ -0,0 +1,9 @@
+console.log("Console command");
+
+/*
+Hai cách comment trong JS
+*/
+
+console.log("Đây là console.log - trả ra dạng thông tin");
+console.warn("Đây là console.warn - trả ra dạng cảnh báo");
+console.error("Đây là console.error - trả ra dạng lỗi");
